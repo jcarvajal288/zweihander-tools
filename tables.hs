a6b6e21c07d7b9f084e946c17dd308b968df951d
Max Carvajal <jcarvajal@crimson.ua.edu>
2019-08-27 21:56:35 -0500

    Added grievous injury table

--- tables.hs
+++ tables.hs
@@ -7,6 +7,7 @@ printMapEntry :: HashMap.Map String String -> String -> String
 printMapEntry hashMap key =
     key ++ " - " ++ Maybe.fromJust (HashMap.lookup key hashMap)
 
+moderateInjuries :: HashMap.Map String String
 moderateInjuries = HashMap.fromList 
      [ ("Black Eye", "Until fully Recuperated, you must flip the resutls to fail Skill Tests which rely on vision.")
      , ("Bruised Ribs", "Until fully Recuperated, reduce your Encumbrance Limit by 3.")
@@ -19,6 +20,7 @@ moderateInjuries = HashMap.fromList
      , ("Strained Groin", "Until fully Recuperated, you cannot use any Movement Actions besides Take Cover or Walk in combat.")
      , ("Twisted Ankle", "Until fully Recuperated, reduce your Movement by 3.") ]
 
+seriousInjuries :: HashMap.Map String String
 seriousInjuries = HashMap.fromList
     [ ("Broken Rib", "Your armor gains the Ruined! Quality.  Until fully Recuperated, you cannot add Skill Ranks to Combat, Brawn or Agility-based Skills.")
     , ("Busted Kneecap", "Until fully Recuperated, any time you fail a Skill Test that relies on Brawn or Agility, you suffer 2d10+2 Physical Peril.")
@@ -31,8 +33,22 @@ seriousInjuries = HashMap.fromList
     , ("Temporary Paralysis", "You are knocked Prone.  Until fully Recuperated, you cannot move as you're paralyzed.")
     , ("Torn Shoulder", "Whatever you are holding in your primary hand gains the Ruined! Quality.  Until fully Recuperated, you start your Turn with 2 less AP.") ]
 
-moderateInjury :: Integer -> String 
-moderateInjury n 
+grievousInjuries :: HashMap.Map String String
+grievousInjuries = HashMap.fromList
+    [ ("Butchered Leg", "Until fully Recuperated, you cannot move as you're in pain.  You must undergo a successful surgery or suffer the consequences.  Once a Butchered Leg has undergone a failed surgery, you gain the Veteran's Leg Drawback.  If you already have this Drawback, you permanently lose 9% Agility.")
+    , ("Cerebral Contusion", "Until fully Recuperated, you remain unconscious.  You must undergo a successful surgery or suffer the consequences.  Once a Cerebral Contusion has undergone a failed surgery, you gain the Dunderhead Drawback.  If you already have this Drawback, you permanently lose 9% Intelligence.")
+    , ("Detached Ear", "Until fully Recuperated, you cannot hear as you're in pain.  You must undergo a successful surgery or suffer the consequences.  Once a Detached Ear has undergone a failed surgery, you suffer the Crop Ear Drawback.  If you already have this Drawback, you permanently lose 9% Perception.")
+    , ("Maimed Foot", "Until fully Recuperated, you cannot use any weapon with an Encumbrance Value of 2 or more, as you're in pain.  You must undergo a successful surgery or suffer the consequences.  Once a Maimed Foot has undergone a failed surgery, you lose 1d6-1 toes.  For every toe lost, you permanently lose 1% Brawn.  If you lose all toes, you gain the Veteran's Boot Drawback.  If you already have this Drawback, you permanently lose 9% Brawn.")
+    , ("Mangled Organ", "Until fully Recuperated, you remain Incapacitated!. You must undergo a successful surgery or suffer the consequences.  Once a Mangled Organ has undergone a failed surgery, you permanetnly gain the Eunuch Drawback.  If you already have this Drawback, you permanently lose 9% Fellowship.")
+    , ("Mutilated Hand", "Until fully Recuperated, you cannot use your primary hand as you're in pain.  You must undergo a successful surgery or suffer the consequences.  Once a Mutilated Hand has undergone a failed surgery, you lose 1d6-1 fingers.  For every finger lost, you permanently lose 1% Agility.  If you lose all fingers, you gain the Veteran's Hand Drawback.  If you already have this Drawback, you permanently lose 9% Agility.")
+    , ("Mutilated Nose", "Until fully Recuperated, you cannot smell as you're in pain.  You must undergo a successful surgery or suffer the consequences.  Once a Mutiliated Nose has undergone a failed surgery, you permanently must flip the results to fail all Skill Tests which rely on smell and taste.  If you already have this Drawback, you permanently lose 9% Perception.")
+    , ("Punctured Lung", "Until fully Recuperated, you remain unconscious.  You must undergo a successful surgery or suffer the consequences.  Once a Punctured Lung has undergone a failed surgery, you permanently gain the Weak Lungs Drawback.  If you already have this Drawback, you permanently lose 9% Willpower.")
+    , ("Splintered Elbow", "Until fully Recuperated, you cannot use one of your arms as you're in pain.  You must undergo a successful surgery or suffer the consequences.  Once a Splintered Elbow has undergone a failed surgery, you can no longer use two-handed ranged weapons.  If you already have this Drawback, you permannetly lose 9% Combat.")
+    , ("Vitreous Hemmorrhage", "Until fully Recuperated, you cannot see as you're blinded.  You must undergo a successful surgery or suffer the consequences.  Once a Vitreous Hemmorrhage has undergone a failed surgery, you gain the Black Cataract Drawback.  If you already have this Drawback, you permanently lose 9% Perception.")
+    , ("Severed Artery", "An arterial spray of blood marks your doom; you are instantly Slain!") ]
+
+rollModerateInjury :: Integer -> String 
+rollModerateInjury n 
     | elem n [1..8]    = "Fortune's Mercy! - Ignore Injury, keep your Fate Point and continue fighting onwards!"
     | elem n [9..17]   = printModerateInjury "Black Eye"
     | elem n [17..25]  = printModerateInjury "Bruised Ribs"
@@ -48,8 +64,8 @@ moderateInjury n
     | otherwise = error ("out of range die roll: " ++ (show n))
     where printModerateInjury = printMapEntry moderateInjuries
 
-seriousInjury :: Integer -> String
-seriousInjury n 
+rollSeriousInjury :: Integer -> String
+rollSeriousInjury n 
     | elem n [1..8]    = "Fortune's Mercy! - Ignore Injury, keep your Fate Point and continue fighting onwards!"
     | elem n [9..17]   = printSeriousInjury "Broken Rib"
     | elem n [17..25]  = printSeriousInjury "Busted Kneecap"
@@ -64,3 +80,20 @@ seriousInjury n
     | elem n [93..100] = "Misfortune! - Roll on the Grievous Injury table instead!"
     | otherwise = error ("out of range die roll: " ++ (show n))
     where printSeriousInjury = printMapEntry seriousInjuries
+
+rollGrievousInjury :: Integer -> String
+rollGrievousInjury n 
+    | elem n [1..8]    = "Fortune's Mercy! - Ignore Injury, keep your Fate Point and continue fighting onwards!"
+    | elem n [9..17]   = printGrievousInjury "Butchered Leg"
+    | elem n [17..25]  = printGrievousInjury "Cerebral Contusion"
+    | elem n [26..33]  = printGrievousInjury "Detached Ear"
+    | elem n [34..41]  = printGrievousInjury "Maimed Foot"
+    | elem n [42..49]  = printGrievousInjury "Mangled Organ"
+    | elem n [50..58]  = printGrievousInjury "Mutilated Hand"
+    | elem n [59..67]  = printGrievousInjury "Mutilated Nose"
+    | elem n [68..76]  = printGrievousInjury "Punctured Lung"
+    | elem n [77..85]  = printGrievousInjury "Splintered Elbow"
+    | elem n [86..92]  = printGrievousInjury "Vitreous Hemmorrhage"
+    | elem n [93..100] = printGrievousInjury "Severed Artery"
+    | otherwise = error ("out of range die roll: " ++ (show n))
+    where printGrievousInjury = printMapEntry grievousInjuries
